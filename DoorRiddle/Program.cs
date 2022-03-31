13c4cc34d3955ec48652567351f3ec5224b1dca4
Miloslav Macůrek <macurek.m@gmail.com>
2022-03-31 19:26:15 +0200

    Move DoorRiddle to net6.0

--- DoorRiddle/Program.cs
+++ DoorRiddle/Program.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
-namespace DoorRiddle
+namespace DoorRiddle;
+
+internal static class Program
 {
-    internal static class Program
-    {
-        public static void Main(string[] args)
+    public static void Main(string[] args)
         /*
          Experimental solution to following statistical riddle.
          You are given 3 doors with treasure behind 1 of them. You choose 1 door, and then the host opens 1 
@@ -15,59 +12,58 @@ namespace DoorRiddle
          You are then given a choice - do you open the door you chose at the beginning, or do you change your choice
          and open the remaining closed door instead? Which option maximizes your chances of finding treasure?
          */
-        {
-            const long cycles = 1000000000;
-            var cpus = Environment.ProcessorCount;
+    {
+        const long cycles = 1000000000;
+        var cpus = Environment.ProcessorCount;
 
-            var watch = Stopwatch.StartNew();
-            var resultWithChange = PlayGameMultiThreaded(cycles, true, cpus);
-            watch.Stop();
-            PrintResultsToConsole(resultWithChange, watch, cycles, true);
+        var watch = Stopwatch.StartNew();
+        var resultWithChange = PlayGameMultiThreaded(cycles, true, cpus);
+        watch.Stop();
+        PrintResultsToConsole(resultWithChange, watch, cycles, true);
             
-            watch.Restart();
-            var resultNoChange = PlayGameMultiThreaded(cycles, false, cpus);
-            watch.Stop();
-            PrintResultsToConsole(resultNoChange, watch, cycles, false);
-        }
+        watch.Restart();
+        var resultNoChange = PlayGameMultiThreaded(cycles, false, cpus);
+        watch.Stop();
+        PrintResultsToConsole(resultNoChange, watch, cycles, false);
+    }
         
-        private static Result PlayGame(long cycles, bool changeChoice)
-        {
-            var doors = new List<bool> { false, true, false };
-            var rnd = new Random();
-            var result = new Result();
+    private static Result PlayGame(long cycles, bool changeChoice)
+    {
+        var doors = new List<bool> { false, true, false };
+        var rnd = new Random();
+        var result = new Result();
 
-            for (var i = 0; i < cycles; i++)
-            {
-                result.AddTry(changeChoice != doors[rnd.Next(3)]);
-            }
-            
-            return result;
+        for (var i = 0; i < cycles; i++)
+        {
+            result.AddTry(changeChoice != doors[rnd.Next(3)]);
         }
+            
+        return result;
+    }
         
-        private static Result PlayGameMultiThreaded(long cycles, bool changeChoice, int cpus)
+    private static Result PlayGameMultiThreaded(long cycles, bool changeChoice, int cpus)
+    {
+        if (cpus == 1)
         {
-            if (cpus == 1)
-            {
-                return PlayGame(cycles, changeChoice);
-            }
-            
-            var interval = cycles / cpus;
-            var inputList = new List<long>();
-            var results = new List<Result>();
-            
-            for (var i = 0; i < cpus; i++) { inputList.Add(interval); }
-            inputList[inputList.Count - 1] += cycles % cpus;
-            
-            Parallel.ForEach(inputList, sub => results.Add(PlayGame(sub, changeChoice)));
-            return Result.Combine(results);
+            return PlayGame(cycles, changeChoice);
         }
+            
+        var interval = cycles / cpus;
+        var inputList = new List<long>();
+        var results = new List<Result>();
+            
+        for (var i = 0; i < cpus; i++) { inputList.Add(interval); }
+        inputList[^1] += cycles % cpus;
+            
+        Parallel.ForEach(inputList, sub => results.Add(PlayGame(sub, changeChoice)));
+        return Result.Combine(results);
+    }
 
-        private static void PrintResultsToConsole(Result result, Stopwatch watch, long cycles, bool changeChoice)
-        {
-            Console.WriteLine();
-            Console.WriteLine($"Change = {changeChoice}. Time elapsed {watch.ElapsedMilliseconds} ms.");
-            Console.WriteLine(result.ToString());
-            Console.WriteLine($"Speed = {cycles / watch.ElapsedMilliseconds / 1000} Miter/s.");
-        }
+    private static void PrintResultsToConsole(Result result, Stopwatch watch, long cycles, bool changeChoice)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Change = {changeChoice}. Time elapsed {watch.ElapsedMilliseconds} ms.");
+        Console.WriteLine(result.ToString());
+        Console.WriteLine($"Speed = {cycles / watch.ElapsedMilliseconds / 1000} Miter/s.");
     }
 }
--- DoorRiddle/Result.cs
+++ DoorRiddle/Result.cs
@@ -1,53 +1,50 @@
-﻿using System.Collections.Generic;
+﻿namespace DoorRiddle;
 
-namespace DoorRiddle
+public class Result
 {
-    public class Result
+    public long Failed { get; private set; }
+    public long Successful { get; private set; }
+    public long Total => Failed + Successful;
+    public double SuccessRate => (double)Successful / Total;
+        
+    public Result()
     {
-        public long Failed { get; private set; }
-        public long Successful { get; private set; }
-        public long Total => Failed + Successful;
-        public double SuccessRate => (double)Successful / Total;
-        
-        public Result()
-        {
-            Failed = 0;
-            Successful = 0;
-        }
+        Failed = 0;
+        Successful = 0;
+    }
 
-        public Result(long failed, long successful)
-        {
-            Failed = failed;
-            Successful = successful;
-        }
+    public Result(long failed, long successful)
+    {
+        Failed = failed;
+        Successful = successful;
+    }
 
-        public static Result Combine(List<Result> results)
-        {
-            long failed = 0;
-            long successful = 0;
+    public static Result Combine(List<Result> results)
+    {
+        long failed = 0;
+        long successful = 0;
             
-            foreach (var result in results)
-            {
-                successful += result.Successful;
-                failed += result.Failed;
-            }
-            
-            return new Result(failed, successful);
-        }
-        
-        public new string ToString() => $"{Successful} successful tries, {Total} total. " +
-                                        $"Success rate {SuccessRate * 100} %.";
-
-        public void AddTry(bool result)
+        foreach (var result in results)
         {
-            if (result)
-            {
-                Successful += 1;
-            }
-            else
-            {
-                Failed += 1; 
-            }
+            successful += result.Successful;
+            failed += result.Failed;
+        }
+            
+        return new Result(failed, successful);
+    }
+        
+    public new string ToString() => $"{Successful} successful tries, {Total} total. " +
+                                    $"Success rate {(SuccessRate * 100):0.0000} %.";
+
+    public void AddTry(bool result)
+    {
+        if (result)
+        {
+            Successful += 1;
+        }
+        else
+        {
+            Failed += 1; 
         }
     }
 }

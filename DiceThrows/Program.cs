ae86e8b6b816348061b109c1eb41a18d48e5250f
Miloslav Macůrek <macurek.m@gmail.com>
2022-03-31 19:31:47 +0200

    Move all the rest to net6.0

--- DiceThrows/Program.cs
+++ DiceThrows/Program.cs
@@ -1,64 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
-namespace DiceThrows
+namespace DiceThrows;
+
+internal static class Program
 {
-    internal static class Program
-    {
-        public static void Main(string[] args)
+    public static void Main(string[] args)
         /*
          Calculate {iterations} sums of {attempts} number of rolls of {dice}-sided dice and return results.
          */
-        {
-            const int dice = 20;
-            const int attempts = 100;
-            const int iterations = 1000000;
-            var cpus = Environment.ProcessorCount;
+    {
+        const int dice = 20;
+        const int attempts = 100;
+        const int iterations = 1000000;
+        var cpus = Environment.ProcessorCount;
 
-            var watch = Stopwatch.StartNew();
-            var result = GetThrowsMultiThreaded(dice, attempts, iterations, cpus);
-            watch.Stop();
+        var watch = Stopwatch.StartNew();
+        var result = GetThrowsMultiThreaded(dice, attempts, iterations, cpus);
+        watch.Stop();
             
-            // result.PrintToConsole();
-            Console.WriteLine($"Execution took {watch.Elapsed}.");
-        }
+        // result.PrintToConsole();
+        Console.WriteLine($"Execution took {watch.ElapsedMilliseconds} ms.");
+    }
         
-        private static Result GetThrows(int dice, int numThrows, int iterations)
-        {
-            var rnd = new Random();
-            var result = new Result();
+    private static Result GetThrows(int dice, int numThrows, int iterations)
+    {
+        var rnd = new Random();
+        var result = new Result();
             
-            for (var i = 0; i < iterations; i++)
+        for (var i = 0; i < iterations; i++)
+        {
+            var number = 0;
+            for (var j = 0; j < numThrows; j++)
             {
-                var number = 0;
-                for (var j = 0; j < numThrows; j++)
-                {
-                    number += rnd.Next(dice) + 1;
-                }
-                result.AddThrow(number);
+                number += rnd.Next(dice) + 1;
             }
-
-            return result;
+            result.AddThrow(number);
         }
 
-        private static Result GetThrowsMultiThreaded(int dice, int numThrows, int iterations, int cpus)
+        return result;
+    }
+
+    private static Result GetThrowsMultiThreaded(int dice, int numThrows, int iterations, int cpus)
+    {
+        if (cpus == 1)
         {
-            if (cpus == 1)
-            {
-                return GetThrows(dice, numThrows, iterations);
-            }
-            
-            var interval = iterations / cpus;
-            var inputList = new List<int>();
-            var results = new List<Result>();
-            
-            for (var i = 0; i < cpus; i++) { inputList.Add(interval); }
-            inputList[inputList.Count - 1] += iterations % cpus;
-            Parallel.ForEach(inputList, sub => results.Add(GetThrows(dice, numThrows, sub)));
-            return Result.Combine(results);
+            return GetThrows(dice, numThrows, iterations);
         }
+            
+        var interval = iterations / cpus;
+        var inputList = new List<int>();
+        var results = new List<Result>();
+            
+        for (var i = 0; i < cpus; i++) { inputList.Add(interval); }
+        inputList[inputList.Count - 1] += iterations % cpus;
+        Parallel.ForEach(inputList, sub => results.Add(GetThrows(dice, numThrows, sub)));
+        return Result.Combine(results);
     }
 }
--- DiceThrows/Result.cs
+++ DiceThrows/Result.cs
@@ -1,48 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿namespace DiceThrows;
 
-namespace DiceThrows
+public class Result
 {
-    public class Result
+    public SortedDictionary<int, int> Throws { get; private set; }
+
+    public Result()
     {
-        public SortedDictionary<int, int> Throws { get; private set; }
-
-        public Result()
-        {
-            Throws = new SortedDictionary<int, int>();
-        }
+        Throws = new SortedDictionary<int, int>();
+    }
         
-        public Result(Dictionary<int, int> throws)
-        {
-            Throws = new SortedDictionary<int, int>(throws);
-        }
+    public Result(Dictionary<int, int> throws)
+    {
+        Throws = new SortedDictionary<int, int>(throws);
+    }
         
-        public void AddThrow(int number)
-        {
-            if (!Throws.ContainsKey(number)) { Throws.Add(number, 1); return; }
-            Throws[number] += 1;
-        }
+    public void AddThrow(int number)
+    {
+        if (!Throws.ContainsKey(number)) { Throws.Add(number, 1); return; }
+        Throws[number] += 1;
+    }
         
-        public static Result Combine(List<Result> results)
+    public static Result Combine(List<Result> results)
+    {
+        var throws = new Dictionary<int, int>();
+        foreach (var result in results)
         {
-            var throws = new Dictionary<int, int>();
-            foreach (var result in results)
-            {
-                throws = throws.Union(result.Throws)
-                    .GroupBy(g => g.Key)
-                    .ToDictionary(pair => pair.Key, pair => pair.First().Value);
-            }
+            throws = throws.Union(result.Throws)
+                .GroupBy(g => g.Key)
+                .ToDictionary(pair => pair.Key, pair => pair.First().Value);
+        }
  
-            return new Result(throws);
-        }
+        return new Result(throws);
+    }
 
-        public void PrintToConsole()
+    public void PrintToConsole()
+    {
+        foreach (var diceThrow in Throws)
         {
-            foreach (var diceThrow in Throws)
-            {
-                Console.WriteLine($"{diceThrow.Key}: {diceThrow.Value}");
-            }
+            Console.WriteLine($"{diceThrow.Key}: {diceThrow.Value}");
         }
     }
 }
